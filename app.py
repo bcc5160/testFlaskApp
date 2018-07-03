a4f9b821b08fe953a659eb424e875e7b44786664
Brittany Chiu <bc.chiu12@gmail.com>
2018-07-03 10:58:29 -0400

    dokcer

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask
 import os 
 import docker
 
+# Docker Setup
+docker_client = docker.from_env()
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     
-    return 'index2'
+    return docker_client.images.list()
 
 if __name__ == '__main__':
     app.run(debug=True)
