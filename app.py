b28d975d2b2b5964c7ee5fe0f525cab7995da1cc
Brittany Chiu <bc.chiu12@gmail.com>
2018-07-09 12:33:33 -0400

    json

--- app.py
+++ app.py
@@ -1,15 +1,26 @@
-from flask import Flask
+from flask import Flask, request
 import os 
 import docker
+
+# Global Variables
+DOCKER_REPO = '34.231.172.160'
+BASE_NAME = 'base'
+BASE_TAG = 'v2'
 
 # Docker Setup
 docker_client = docker.from_env()
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/newApp', methods = ['POST'])
 def index():
-    return 'index'
+    new_name = request.get_json()
+
+    # docker_client.images.pull(DOCKER_REPO, BASE_NAME + ":" + BASE_TAG)
+    os.system('sudo docker pull ' + DOCKER_REPO + '/' + BASE_NAME + ':' + BASE_TAG)
+    os.system('sudo docker tag ' + DOCKER_REPO + '/' + BASE_NAME + ':' + BASE_TAG + ' ' + DOCKER_REPO + '/' + NEW_NAME + ':' + BASE_TAG)
+    os.system('sudo docker push ' + DOCKER_REPO + '/' + new_name + ':' + BASE_TAG)
+    return str(new_name) + ' was created.'
     # return docker_client.images.list()
 
 if __name__ == '__main__':
