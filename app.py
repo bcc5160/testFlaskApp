530691e2f543a4c6d951c39647604ee8cfca65da
Brittany Chiu <bc.chiu12@gmail.com>
2018-07-09 17:00:42 -0400

    dcoker

--- app.py
+++ app.py
@@ -15,11 +15,11 @@ app = Flask(__name__)
 @app.route('/newApp', methods = ['POST'])
 def new_app():
     new_name = request.get_json()
-
-    # docker_client.images.pull(DOCKER_REPO, BASE_NAME + ":" + BASE_TAG)
-    os.system('sudo docker pull ' + DOCKER_REPO + '/' + BASE_NAME + ':' + BASE_TAG)
-    os.system('sudo docker tag ' + DOCKER_REPO + '/' + BASE_NAME + ':' + BASE_TAG + ' ' + DOCKER_REPO + '/' + NEW_NAME + ':' + BASE_TAG)
-    os.system('sudo docker push ' + DOCKER_REPO + '/' + new_name + ':' + BASE_TAG)
+    docker_client.images.pull(DOCKER_REPO + "/" + BASE_NAME + ":" + BASE_TAG)
+    
+    # os.system('sudo docker pull ' + DOCKER_REPO + '/' + BASE_NAME + ':' + BASE_TAG)
+    # os.system('sudo docker tag ' + DOCKER_REPO + '/' + BASE_NAME + ':' + BASE_TAG + ' ' + DOCKER_REPO + '/' + NEW_NAME + ':' + BASE_TAG)
+    # os.system('sudo docker push ' + DOCKER_REPO + '/' + new_name + ':' + BASE_TAG)
     return str(new_name) + ' was created.'
     # return docker_client.images.list()
 
