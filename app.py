f4caf0a3dd5fa581708c297fbad1576e9c94bdf3
Brittany Chiu <bc.chiu12@gmail.com>
2018-07-10 16:11:11 -0400

    reqs

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 import os 
 import docker
+from docker import Client
+import json 
 
 # Global Variables
 DOCKER_REPO = '34.231.172.160'
@@ -8,22 +10,34 @@ BASE_NAME = 'base'
 BASE_TAG = 'v2'
 
 # Docker Setup
-docker_client = docker.from_env()
+docker_client = Client(base_url='http://localhost:2376/')
 
 app = Flask(__name__)
 
 @app.route('/newApp', methods = ['POST'])
 def new_app():
-    new_name = request.get_json()
-    
-    docker_client.images.pull(DOCKER_REPO + "/" + BASE_NAME + ":" + BASE_TAG)
-    
-    image = docker_client.images.get(DOCKER_REPO + "/" + BASE_NAME + ":" + BASE_TAG)
-    image.tag(DOCKER_REPO + "/" + str(new_name) + ":" + BASE_TAG)
+    data = request.get_json()
+    print(data['new_app'])
+    new_name = data['new_app']
 
-    docker_client.images.push(DOCKER_REPO + "/" + str(new_name) + ":" + BASE_TAG)
+    print(type(str(new_name + ':' + BASE_TAG)))
 
-    return str(new_name) + ' was created.'
+    base_app_image = DOCKER_REPO + '/' + BASE_NAME + ':' + BASE_TAG
+    new_app_image = DOCKER_REPO + '/' + str(new_name)
+
+    docker_client.pull(base_app_image + ':' + BASE_TAG)
+    docker_client.tag(base_app_image, new_app_image, BASE_TAG)
+    # docker_client.push(new_app_image + ':' + BASE_TAG, stream=True)
+    response = [line for line in docker_client.push(new_app_image, stream=True)]
+    # docker_client.images.pull(DOCKER_REPO + "/" + BASE_NAME + ":" + BASE_TAG)
+
+    # image = docker_client.images.get(DOCKER_REPO + "/" + BASE_NAME + ":" + BASE_TAG)
+    # image.tag(DOCKER_REPO + "/" + str(new_name) + ":" + BASE_TAG)
+
+    # docker_client.images.push(DOCKER_REPO + "/" + str(new_name) + ":" + BASE_TAG)
+
+    print(response)
+    return new_app_image + ':' + BASE_TAG
 
 
 
@@ -32,5 +46,6 @@ def test_server():
     return "OK."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
+~                                                    
